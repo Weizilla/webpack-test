e786133b756db0680d15eab9e18f1a7b1eecde26
Wei Yang <wei@weizilla.com>
2016-02-25 21:02:10 -0600

    add angular

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+class HelloController {
+    constructor() {
+        self._world = "WORLD";
+    }
+
+    world() {
+        return self._world;
+    }
+};
+
+export default HelloController
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Greeting from "./greeting.js"
 import DateTime from "./datetime.js"
+import HelloController from "./hello.js"
+import angular from "angular"
 
 var h1 = document.querySelector("h1");
 h1.textContent = new Greeting();
 
 var h2 = document.querySelector("h2");
 h2.textContent = new DateTime();
+
+angular.module("app", [])
+    .controller("HelloController", HelloController);
