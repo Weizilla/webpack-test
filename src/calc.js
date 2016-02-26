251f9683d499b23b9b8dcbe75913c0d44d4c8d2f
Wei Yang <wei@weizilla.com>
2016-02-25 21:27:20 -0600

    add calc angular example

---
+++ src/calc.js
@@ -0,0 +1,12 @@
+class CalculatorController {
+    constructor() {
+        this.a = 10;
+        this.b = 20;
+    }
+
+    calculate() {
+        this.result = this.a + this.b;
+    }
+}
+
+export default CalculatorController
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Greeting from "./greeting.js"
 import DateTime from "./datetime.js"
 import HelloController from "./hello.js"
+import CalculatorController from "./calc.js"
 import angular from "angular"
 
 var h1 = document.querySelector("h1");
@@ -10,4 +11,5 @@ var h2 = document.querySelector("h2");
 h2.textContent = new DateTime();
 
 angular.module("app", [])
-    .controller("HelloController", HelloController);
+    .controller("HelloController", HelloController)
+    .controller("CalculatorController", CalculatorController);
